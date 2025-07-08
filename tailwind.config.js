559b22004bdfe1db0bce598433f43c613ef4162e
sheaksadi <sheaksadi123456@gmail.com>
2025-07-09 02:26:10 +0600

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./src/**/*.vue",
+    "./src/*.vue",
+    "./src/App.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+    "./error.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
